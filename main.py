ded45534e13e0436674cef2fa0ac1ae49d63700a
vuyopapiyana <99693948+vuyopapiyana@users.noreply.github.com>
2025-11-22 17:08:11 +0200

    serving fastapi and inngest server, RAG Inngest PDF func created

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from pdf-production-rag!")
+# --------- Imports -------------
+from fastapi import FastAPI
+import logging
+import inngest
+from inngest import Function
+from inngest import TriggerEvent
+import inngest.fast_api
+from inngest.experimental import ai
+import uuid
+import datetime
+import os
+from dotenv import load_dotenv
+# ------- Code logic --------------
+load_dotenv()
 
+inngest_client = inngest.Inngest(
+    app_id="rag_app",
+    logger=logging.getLogger("uvicorn"),
+    is_production=False,
+    serializer=inngest.PydanticSerializer(),
+)
 
-if __name__ == "__main__":
-    main()
+@inngest_client.create_function(
+    fn_id= "RAG: Inngest PDF",
+    trigger=inngest.TriggerEvent(event='rag/inngest_pdf') # type: ignore[arg-type]
+)
+async def inngest_pdf(ctx: inngest.Context):
+    return {"hello": "world"}
+
+# ------ Serving app ---------
+app = FastAPI()
+inngest.fast_api.serve(app=app,client=inngest_client, functions=[inngest_pdf]) # type: ignore[arg-type]
